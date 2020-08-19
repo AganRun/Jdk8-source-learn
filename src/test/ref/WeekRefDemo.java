7cd795f21eca3a89167a8675c201532a600c5f8f
AganRun <1379154067@qq.com>
2020-08-19 22:46:30 +0800

    feat(WeekReference)

--- src/test/ref/WeekRefDemo.java
+++ src/test/ref/WeekRefDemo.java
@@ -1,12 +1,21 @@
 package test.ref;
 
+import java.lang.ref.WeakReference;
+
 public class WeekRefDemo {
 
     public static void main(String[] args) {
-
+        Person p = new Person("Job");
+        WeakReference r = new WeakReference(p);
+        System.out.println(r.get());  //job
+        System.gc();    //系统回收
+        System.out.println(r.get());  //job   此时还有强引用
+        p = null;   //取消强引用
+        System.gc();    //再回收
+        System.out.println(r.get());  //null
     }
 
-    class Person {
+    static class Person {
         String name;
         Person(String name) {
             this.name = name;
