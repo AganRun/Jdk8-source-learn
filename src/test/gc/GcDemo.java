1975d643483024b2ea4bf37672f780deed2565a5
AganRun <1379154067@qq.com>
2020-11-25 18:22:53 +0800

    feat(gc) 查看GC情况

---
+++ src/test/gc/GcDemo.java
@@ -0,0 +1,12 @@
+package test.gc;
+
+public class GcDemo {
+
+    //运行参数：-XX:+PrintGCDetails
+    public static void main(String[] args) {
+        byte[] allocation1, allocation2;
+        allocation1 = new byte[30900 * 1024];
+        allocation2 = new byte[900 * 1024];
+    }
+
+}
